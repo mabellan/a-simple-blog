0c15d9218d4b75432ed75e9c256bdb1a16059c3d
Miguel Abellan <mabellan1996@gmail.com>
2022-12-21 23:17:39 +0100

    Added unit testing to the project

---
+++ simple-blog-test/Post/PostTest.cs
@@ -0,0 +1,22 @@
+﻿namespace simple_blog_test;
+using simple_blog;
+using simple_blog.Domain.Post.Model;
+
+[TestClass]
+public class PostTest
+{
+    [TestMethod]
+    public void Creates_New_Post_And_Validates()
+    {
+        // Arrange
+        string title = "This is a title test";
+        string body = "This is a body test";
+        Post expected = new Post(title, body);
+
+        // Assert
+        Assert.AreEqual(expected.Title, title);
+        Assert.AreEqual(expected.Body, body);
+        Assert.AreEqual(expected.IsDraft, true);
+
+    }
+}
